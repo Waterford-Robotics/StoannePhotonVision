24fde417246e90d1168cfc5569d9a390fce8159e
Juan <140850008+Jo0Ann@users.noreply.github.com>
2024-02-14 17:07:31 -0700

    Update DriveArcadeStyle.java
    added documentation

--- PhotonVision/src/main/java/frc/robot/commands/DriveArcadeStyle.java
+++ PhotonVision/src/main/java/frc/robot/commands/DriveArcadeStyle.java
@@ -11,6 +11,7 @@ public class DriveArcadeStyle extends Command{
     private DoubleSupplier m_forwardSpeedSupplier;
     private DoubleSupplier m_turnSpeedSupplier;
 
+    // Declaring your drive train
     public DriveArcadeStyle(DriveTrain drivetrain, DoubleSupplier forwardSpeedSupplier, DoubleSupplier turnSpeedSupplier){
         addRequirements(drivetrain);
         m_drivetrain = drivetrain;
@@ -18,19 +19,23 @@ public class DriveArcadeStyle extends Command{
         m_turnSpeedSupplier = turnSpeedSupplier;
     }
 
+    // Initializing the method
     @Override
     public void initialize(){}
 
+    // Executing code to make drive train move
     @Override
     public void execute(){
         m_drivetrain.drive(m_forwardSpeedSupplier.getAsDouble(), m_turnSpeedSupplier.getAsDouble());
     }
 
+    // Stop driving when input is interrupted
     @Override
     public void end(boolean interrupted){
         m_drivetrain.stop();
     }
 
+    // Determines when drive code is finished
     @Override
     public boolean isFinished(){
         return false;
